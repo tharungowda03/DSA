72722f24d38e107efb02f11e6696e7e5053f76a8
BOLE THARUN <gowdatharun692@gmail.com>
2026-09-24 15:45:30 +0530

    Time: 0 ms (100%), Space: 19.2 MB (78.24%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        a = m - 1
+        b = n - 1
+        c = m + n - 1
+        while b >= 0:
+            if a >= 0 and nums1[a] > nums2[b]:
+                nums1[c] = nums1[a]
+                a -= 1
+            else:
+                nums1[c] = nums2[b]
+                b -= 1
+            c -= 1
